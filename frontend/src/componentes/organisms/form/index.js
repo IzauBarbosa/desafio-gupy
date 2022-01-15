27f8c51b9bcb595cb33783c2ee6812fe49d4e2eb
Izau Barbosa <izau.dev@gmail.com>
2022-01-15 18:53:20 -0300

    created organisms 'form', no style

---
+++ frontend/src/componentes/organisms/form/index.js
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+import { Styles } from './styles'
+import { Button } from '../../atoms';
+import { InputDefault, InputRadio } from '../../molecules';
+import requestToAPI from '../../../services/request';
+
+export const Form = ({ request, fields, callback  }) => {
+
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const { method, context, endpoint, data } = request;
+    const response = await requestToAPI({ 
+      method, context, endpoint, data
+    }, setLoading);
+
+    callback(response);
+  }
+
+  return (
+    <Styles>
+      <form onSubmit={handleSubmit}>
+        {(fields || []).map((field, index) => {
+          const { id, name, label, type, onChange, value, group } = field;
+
+          switch (type) {
+            case 'radio':
+
+              return (
+                <InputRadio key={`input-radio-${index}`} id={id} name={name} label={label} onChange={onChange} group={group} />
+              );
+            default:
+              
+              return (
+                <InputDefault key={`input-default-${index}`} id={id} label={label} type={type} onChange={onChange} value={value} />
+              );
+          }
+        })}
+        <Button type="submit" disabled={loading} loading={loading}>Enviar</Button>
+      </form>
+    </Styles>
+  )
+}
